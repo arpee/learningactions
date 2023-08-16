efac5d31ee7b6f359b9414eb1986f41e77de4318
arpee <richard@saito.tech>
2023-08-16 23:46:16 +1000

    fixing spacing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
-    env: {
-      browser: true,
-      commonjs: true,
-      es2021: true,
-      node: true,
-    },
-    extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended",
-      "prettier",
-    ],
-    globals: {
-      Atomics: "readonly",
-      SharedArrayBuffer: "readonly",
-    },
-    parserOptions: {
-      ecmaVersion: 2018,
-    },
-    rules: {
-      indent: ["error", 2],
-    },
-  };
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    indent: ["error", 2],
+  },
+};
